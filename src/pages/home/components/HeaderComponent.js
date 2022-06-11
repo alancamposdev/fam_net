8996d16f16f2a0fee3fd8954bb8e735e42be71bd
AlanCampos <alancamposdeveloper@gmail.com>
2022-06-11 14:55:17 -0400

    iniciando a home do projeto

---
+++ src/pages/home/components/HeaderComponent.js
@@ -0,0 +1,20 @@
+const Header = () => {
+  return(
+    <header className="header">
+          <div className="header_container">
+
+            <div>
+              <a href="#">FamNet</a>
+            </div>
+
+            <div>
+              <button>Postar</button>
+              <span>img1</span>
+              <span>img2</span>          
+            </div>
+
+          </div>
+      </header>
+  );
+};
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+/**Estilos */
+import "./home/header.css";
+import "./home/main.css";
+//componentes
+import Header from "./home/components/HeaderComponent";
+
+function Home(){
+  return(
+    <div>
+      <Header/>
+      <main className="main">
+          <div className="navbar">Navbar</div>
+          <div className="feed">Feed</div>
+      </main>
+    </div>
+  )
+}
+export default Home;
